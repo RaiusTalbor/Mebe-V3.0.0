8790f25fd8746a842a0cccba1bfd11922f7c2a89
talbor_ <194424255+RaiusTalbor@users.noreply.github.com>
2026-01-06 11:56:19 +0100

    Klasse Strecke hinzugefügt
    ;

--- Fahrer.py
+++ Fahrer.py
@@ -1,9 +1,45 @@
 # Motorsportmeisterschaftsberechner
 # Mebe V3.0.0
 
-#Variablen
+import Daten
 
-#getter
-#setter
-#speichern
-#laden
+class Fahrer:
+    
+    #Variablen
+    pfad = ""
+    name= ""
+    leistung = 0
+    wendigkeit = 0
+
+    def __init__(self):
+        self.pfad = ""
+        self.name= ""
+        self.leistung = 0
+        self.wendigkeit = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.pfad = "Datenbank/Fahrzeuge/" + name + ".dat"
+        self.leistung = 0
+        self.wendigkeit = 0
+
+    def __init__(self, name, leistung, wendigkeit):
+        self.name = name
+        self.pfad = "Datenbank/Fahrzeuge/" + name + ".dat"
+        self.leistung = leistung
+        self.wendigkeit = wendigkeit
+
+    #getter
+    #setter
+    def setpfad(self, name):
+        self.pfad = "Datenbank/Fahrzeuge/" + name + ".dat"
+
+    #speichern
+    def speichern(self):
+        Daten.schreiben(self.pfad, [self.leistung, self.wendigkeit])
+
+    #laden
+    def laden(self):
+        attributliste = Daten.lesen(self.pfad)
+        self.leistung = attributliste[0]
+        self.wendigkeit = attributliste[1]
--- Strecke.py
+++ Strecke.py
@@ -1,9 +1,60 @@
 # Motorsportmeisterschaftsberechner
 # Mebe V3.0.0
 
-#Variablen
+import Daten
 
-#getter
-#setter
-#speichern
-#laden
+class Strecke:
+    
+    #Variablen
+    pfad = ""
+    name= ""
+    rekordhalter = ""
+    pfadrekordhalter = ""
+    streckentyp = 0
+    schwierigkeit = 0
+
+    def __init__(self):
+        self.pfad = ""
+        self.name= ""
+        self.rekordhalter = ""
+        self.pfadrekordhalter = ""
+        self.streckentyp = 0
+        self.schwierigkeit = 0
+
+    def __init__(self, name):
+        self.name= name
+        self.pfad = "Datenbank/Strecken/" + name + ".dat"
+        self.rekordhalter = ""
+        self.pfadrekordhalter = ""
+        self.streckentyp = 0
+        self.schwierigkeit = 0
+
+    def __init__(self, name, rekordhalter, streckentyp, schwierigkeit):
+        self.name= name
+        self.pfad = "Datenbank/Strecken/" + name + ".dat"
+        self.rekordhalter = rekordhalter
+        self.pfadrekordhalter = "Datenbank/Fahrer/" + rekordhalter + ".dat"
+        self.streckentyp = streckentyp
+        self.schwierigkeit = schwierigkeit
+
+    #getter
+    #setter
+    def setpfad(self, name):
+        self.pfad = "Datenbank/Strecken/" + name + ".dat"
+
+    def setrekordhalter(self, übergabe):
+        self.rekordhalter = übergabe
+        self.pfadrekordhalter = "Datenbank/Fahrer/" + übergabe + ".dat"
+
+    #speichern
+    def speichern(self):
+        Daten.schreiben(self.pfad, [self.rekordhalter, self.streckentyp, self.schwierigkeit])
+        #PRÜFEN, OB REKORDHALTER ALS NAME ODER ALS PFAD
+
+    #laden
+    def laden(self):
+        attributliste = Daten.lesen(self.pfad)
+        self.rekordhalter = attributliste[0]
+        self.pfadrekordhalter = "Datenbank/Fahrer/" + self.rekordhalter + ".dat"
+        self.streckentyp = attributliste[1]
+        self.schwierigkeit = attributliste[2]
