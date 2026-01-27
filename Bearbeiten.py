46db530f0a140a5c335a65fe2ef33a6a63efc4dd
talbor_ <194424255+RaiusTalbor@users.noreply.github.com>
2026-01-27 12:57:42 +0100

    Vorbereitung auf Bearbeiten
    Anpassung an neue Struktur;

--- Bearbeiten.py
+++ Bearbeiten.py
@@ -2,10 +2,7 @@
 # Mebe V3.0.0
 # Untermenü bearbeiten - Meisterschaften, Strecken, Fahrer, Fahrzeuge editieren
 
-import time
 from tkinter import *
-import Daten        #Lesen, Schreiben von Dateien
-import os
 
 #---
 import Erstellen
@@ -13,36 +10,12 @@ import ErstelleStrecke
 import ErstelleFahrer
 import ErstelleFahrzeug
 
-def test():
-    pass
+from Anzeige import prozess
 
 def bearbeiten():
-    fensterBearbeiten = Toplevel()
-    fensterBearbeiten.title("Bearbeiten - Mebe V3.0.0")
-    fensterBearbeiten.geometry("800x600")
+    prozess.hinzufügenButton("Bearbeiten - bitte Auswählen")
 
-    #Frames
-    frameInfo = Frame(master=fensterBearbeiten)
-    frameButtons = Frame(master=fensterBearbeiten)
-    frameInfo.pack()
-    frameButtons.pack()
-
-    labelTitelErstellen = Label(master=frameInfo,
-                    text="Bearbeiten von Daten - bitte auswählen",
-                    font=('', 15))
-    labelTitelErstellen.pack()
-
-    buttonMeisterschaft = Button (frameButtons, text = "Meisterschaft bearbeiten", command = Erstellen.bearbeiten)
-    buttonMeisterschaft.pack(side=LEFT, anchor=N, padx= 20, pady = 20)
-
-    buttonStrecke = Button (frameButtons, text = "Strecke bearbeiten", command = ErstelleStrecke.bearbeiten)
-    buttonStrecke.pack(side=LEFT, anchor=N, padx= 20, pady = 20)
-
-    buttonFahrer = Button (frameButtons, text = "Fahrer bearbeiten", command = ErstelleFahrer.bearbeiten)
-    buttonFahrer.pack(side=LEFT, anchor=N, padx= 20, pady = 20)
-
-    buttonFahrzeug = Button (frameButtons, text = "Fahrzeug bearbeiten", command = ErstelleFahrzeug.bearbeiten)
-    buttonFahrzeug.pack(side=LEFT, anchor=N, padx= 20, pady = 20)
-
-    buttonZurück = Button (frameButtons, text = "Zurück", command = fensterBearbeiten.destroy)
-    buttonZurück.pack(side=LEFT, anchor=N, padx= 20, pady = 20)
+    prozess.hinzufügenButton("Meisterschaft bearbeiten". Erstellen.bearbeiten)
+    prozess.hinzufügenButton("Strecke bearbeiten". Erstellen.bearbeiten)
+    prozess.hinzufügenButton("Fahrer bearbeiten". ErstelleFahrer.bearbeiten)
+    prozess.hinzufügenButton("Fahrzeug bearbeiten". ErstelleFahrzeug.bearbeiten)
